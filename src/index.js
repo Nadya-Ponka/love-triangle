9aebc4e6eb8c9f32b28ebdc879fbb433654a559e
Nadya-Ponka <ponkratova@gmail.com>
2018-02-19 23:36:08 +0300

    Solved

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let triangles = 0;
+  preferences.map(function(x, index) {
+    let second, third;
+    if ((x-1) != index) {
+    second = preferences[x-1];
+    if (preferences[second-1] != second) {
+    third = preferences[second-1];
+    if (preferences[third-1] != third && index == (third-1)) {triangles++;}
+} }  });
+  return Math.round(triangles/3);
 };
